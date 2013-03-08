14309f721384b3c06692d73e9588dfb15998ad6c
안형민 <wakehm@gmail.com>
2013-03-08 19:40:07 +0900

    git test2222

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ def main3():
 def main4():
     print "ISSUES TEST"
 
+def main():
+    print "hello, world"
+
+def main2():
+    print "this is main 2"
+    
+def main3():
+    print "this is other main" // modify choiycdev
+
+def main4():
+    print "ISSUES TEST"
+
 if __name__ == '__main__':
     main()
 
