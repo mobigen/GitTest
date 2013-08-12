d352ca2af09b13aa27c32a08e240e3fc5df98794
김성훈 <arton7278@mobigen.com>
2013-08-12 10:09:33 +0200

    test.js변경

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
 var buf = new Buffer(256);
 var len =  buf.write('abcde',4,'utf8');
-console.log( buf.toString('utf8',0,len));
-len = buf.write('abcde',2,'utf8');
 console.log(buf.toString('utf8',0,len));
