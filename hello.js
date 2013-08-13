cdb3fb0efef5e77ec9a3f81c6d78c61e74a85c94
김성훈 <arton7278@mobigen.com>
2013-08-13 03:11:50 +0200

    pushTest

--- hello.js
+++ hello.js
@@ -2,5 +2,9 @@ var util = require('util');
 console.log("hello Node.js");
 
 console.log("hello Node.js");
-util.puts('hello world!!');
+console.log("hello Node.js");
+console.log("hello Node.js");
+console.log("hello Node.js");
+console.log("hello Node.js");
+console.log("hello Node.js");
 util.puts('hello world!!');
--- test.js
+++ test.js
@@ -2,4 +2,4 @@ var buf = new Buffer(256);
 var len =  buf.write('abcde',4,'utf8');
 console.log("길이 : "+len);
 console.log(buf.toString('utf8',0,len));
-console.log();
+
