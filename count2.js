fcf6798e4ee5c044ef6c942006e42549458bd1a6
최영철 <veldise@gmail.com>
2014-06-24 11:40:10 +0900

    submodule test 수정

--- count2.js
+++ count2.js
@@ -4,7 +4,7 @@ var start = new Date().getTime();
 // something
 var end = new Date().getTime();
 
-console.log('start_time :'+start);
-console.log('end_time :'+end);
+console.log('start_time :' + start);
+console.log('end_time :' + end);
 console.log('time :'+ (end - start));
 
