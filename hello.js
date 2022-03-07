e8ab11460f5f0c31d4ec6b4137e44c0bf0e572e9
anhm <wakehm@gmail.com>
2022-03-07 15:54:15 +0900

    remove

--- hello.js
+++
@@ -1,5 +0,0 @@
-// comment
-console.log("hi Node.js");
-console.log("hello Node.js");
-console.log("hi Node.js");
-// pull request 테스트
--- main2.py
+++
@@ -1,4 +0,0 @@
-print "hello python"
-print "hello python"
-print "hello python"
-print "hello python"
